01f9d299e1bf1fed261d155f36b4db2c8072b985
santosbernardogabriel-wq <santos.bernardo.gabriel@escola.pr.gov.br>
2025-10-27 07:52:45 -0300

    Create ex03_for.c

---
+++ ex03_for.c
@@ -0,0 +1,13 @@
+ALGORITMO Tabuada
+INÍCIO
+  VAR N, i: INTEIRO
+
+  ESCREVA "Digite o número para ver a tabuada:"
+  LEIA N
+
+  ESCREVA "Tabuada do ", N, ":"
+  
+  PARA i DE 1 ATÉ 10 FAÇA
+    ESCREVA N, " x ", i, " = ", (N * i)
+  FIM_PARA
+FIM
